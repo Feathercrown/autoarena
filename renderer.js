8e4b5aab6db020f738833bf545cf6585a774a939
Cubic-Zirconia <crystaltheandroid@gmail.com>
2020-01-18 01:11:17 -0500

    Netcode activated

--- renderer.js
+++ renderer.js
@@ -6,30 +6,37 @@
 // process.
 
 var config = {
-    serverUrl: "ws://localhost:80"
+    serverUrl: "ws://localhost:80", //TODO: Make this configurable for the end user via in-app text input
+    mapSize: 33 //TODO: Make this get map size from server instead of defining it here in client
 };
 
+// Initialization
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
-
-console.log("debug");
-
 var socket = new WebSocket(config.serverUrl);
 
-// Socket connection handling
-socket.onerror = function(){
-    alert("ERROR");
-};
-socket.onclose = function(){
-    alert("SOCKET CLOSED");
-};
-socket.onopen = function(){
-    console.log("Open");
-    socket.send('{"message":"test from client"}'); // TX
+var game = {
+    state:{
+        board: (()=>{new Array(config.mapSize).fill(new Array(config.mapSize).fill(new Tile()))})(), // Create a 2d array based on config value
+        scores: [0,0,0,0] // Rudimentary defaults
+    },
+    send: function(msgType, data){
+        socket.send(JSON.stringify({type:msgType, data:data}));
+    },
+    recieve: function(msgType, data){
+        console.log(msgType);
+        console.log(data);
+    }
 };
 
-//Actual good stuff
-socket.onmessage = function(payload){ // RX
-    console.log("Message:\n");
-    console.log(payload);
+//Socket connection handling
+socket.onerror = function(){alert("ERROR");};
+socket.onclose = function(){alert("SOCKET CLOSED");};
+socket.onmessage = function(payload){
+    var msg = JSON.parse(payload);
+    game.recieve(msg.type, msg.data);
+};
+socket.onopen = function(){
+    console.log("Connecting to server");
+    game.send("init",{stage:0});
 };
--- ws-server.js
+++ ws-server.js
@@ -6,23 +6,29 @@ var server = {
 	socket:new WebSocket.Server({
         port:80
     }),
-    clientIDs:[],
-    clients:{}
+    clients: new Map()
 };
+
 server.socket.on("connection",(client,request)=>{
     //Register client
     var id = request.headers['sec-websocket-key'];
     console.log("Connection made; key/id: "+id);
-    server.clientIDs.push(id);
-    server.clients[id]=client;
+    server.clients.set(id,client);
 
     client.on("message",(msg)=>{
-        recieveMessage(id,msg);
+        console.log(id,msg);
+        var parsedMsg = JSON.parse(msg);
+        recieveMessage(id, server.clients.get(id), parsedMsg.type, parsedMsg.data);
     });
 });
 
-function recieveMessage(id,msg){
-    console.log(id,msg);
-    var msg = JSON.parse(msg);
-    var client = server.clients[id];
+function recieveMessage(clientId, client, msgType, data){
+    /**nintendo**/ switch(msgType){ // *Delphino Plaza intensifies*
+        case "init1":
+            //Code goes here
+            break;
+        default:
+            console.log("Invalid message type recieved");
+            break;
+    }
 }
